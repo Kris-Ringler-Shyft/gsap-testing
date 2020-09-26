cd9d7f54de1b17ae7689f4838f908fbcfe293a26
Kris Ringler <krisringler@kriss-air.attlocal.net>
2020-09-25 18:16:04 -0700

    some fun animations added

--- src/App.js
+++ src/App.js
@@ -2,28 +2,17 @@ import React, { useRef, useEffect } from 'react';
 import { TweenMax, Power3 } from 'gsap'
 import './App.css';
 import Modal from './modal';
+import Header from './Header';
 
 
 function App() {
   const video = {
     url: "http://www.youtube.com/embed/xDMP3i36naA"
   }
-  // let logoItem = useRef(null)
-  // console.log(logoItem);
-  // useEffect(() => {
-  //   TweenMax.to(
-  //     logoItem,
-  //     .8,
-  //     {
-  //       opacity: 1,
-  //       y: -20,
-  //       ease: Power3.easeOut
-  //     }
-  //   )
-  // }, [])
+
   return (
     <div className="App">
-
+      <Header />
       <Modal videoSrc={video.url} />
     </div>
   );
